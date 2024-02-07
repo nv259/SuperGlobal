f76b20a82a7b8860f8149a3e013f752740822f32
nv259 <nguyenvietnhat.03@gmail.com>
2024-02-08 01:27:06 +0700

    build baseline for lecha

--- app.py
+++ app.py
@@ -1,27 +1,65 @@
 import os
 
 from flask import Flask, render_template, request, jsonify
+# from flask_cors import CORS
+import cv2 
 
 
 # create flask instance
 app = Flask(__name__)
 
-INDEX = os.path.join(os.path.dirname(__file__), 'index.csv')
+# Apply flask CORS
+# CORS(app) 
+# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = "static"
 
+# Load model (checkpoint or else)
+model = ...
+
+# create FeatureExtractor class (optional)
+# import feature_extract or else
+
+# Load database (db.npy)
+extracted_features = ...
 
 # main route
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        image = request.files['file']
+        
+        if image:
+            # Save image
+            path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+            image.save(path_to_save)
+            
+            # convert to cv2
+            query_image = cv2.imread(path_to_save)
+            
+            # Extract feature for query_image
+            # query_feature = feature_extract(query_image, )
+            
+            # Compute similarity
+            
+            
+            # Sort ranked list + slice for k elements (optional)
+            ranked_list = ['./static/1.jpg', './static/2.jpg', './static/3.jpg', './static/4.jpg', './static/5.jpg']
+            
+        return render_template("index.html", msg="retrieve successfully!", ranked_list=ranked_list)
+        
     return render_template('index.html')
 
 
-# search route
-@app.route('/search', methods=['POST'])
-def search():
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='2503')
 
-    if request.method == "POST":
+# # search route
+# @app.route('/search', methods=['POST'])
+# def search():
 
-        RESULTS_ARRAY = []
-        # get url
-        image_url = request.files('file')
+#     if request.method == "POST":
+
+#         RESULTS_ARRAY = []
+#         # get url
+#         image_url = request.files('file')
 
